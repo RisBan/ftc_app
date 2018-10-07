4a0a2076ae14d42eceb9515c8c80d02ab59e0048
RisBan <43934791+RisBan@users.noreply.github.com>
2018-10-07 16:25:01 -0400

    Renamed title. Is enabled.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp_Mode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp_Mode.java
@@ -50,8 +50,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
+@TeleOp(name="Test OpMode", group="Iterative Opmode")
+
 public class TestOp_Mode extends OpMode
 {
     // Declare OpMode members.
